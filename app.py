f0363237b1091998147b07770fcba854b4424d35
yahoryakubovich <officialegoryakubovich@gmail.com>
2023-10-21 08:27:34 +0300

    Prototype

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+# Подключение к MongoDB
+mongo_uri = os.environ.get('MONGO_URI')
+client = MongoClient(mongo_uri)
+db = client.mydb
+collection = db.mydata
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route('/create', methods=['POST'])
+def create_value():
+    data = request.get_json()
+    key = data.get('key')
+    value = data.get('value')
+    result = collection.insert_one({'key': key, 'value': value})
+    return jsonify({'message': 'Value created successfully', 'id': str(result.inserted_id)})
+
+
+@app.route('/update', methods=['PUT'])
+def update_value():
+    data = request.get_json()
+    key = data.get('key')
+    new_value = data.get('new_value')
+    result = collection.update_one({'key': key}, {'$set': {'value': new_value}})
+    if result.modified_count == 0:
+        return jsonify({'message': 'Key not found'})
+    return jsonify({'message': 'Value updated successfully'})
+
+
+@app.route('/read/<key>', methods=['GET'])
+def read_value(key):
+    result = collection.find_one({'key': key})
+    if result:
+        return jsonify({'key': result['key'], 'value': result['value']})
+    return jsonify({'message': 'Key not found'})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
